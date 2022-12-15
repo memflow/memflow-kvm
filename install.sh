82ad0eff3e6c11ceca6f5d837c282c6da5a887b5
ko1N <ko1N1337@gmail.com>
2022-12-15 20:18:09 +0100

    Added udev rule generation, removed old install.sh script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cargo build --release --all-features
-if [[ ! -d ~/.local/lib/memflow ]]; then
-    mkdir -p ~/.local/lib/memflow
-fi
-cp target/release/libmemflow_kvm.so ~/.local/lib/memflow
